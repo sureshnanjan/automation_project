a3a87f97a2c2bb5f1b882aa4f614afcc44f4c9c5
Tester One <testerone@automation.com>
2021-10-31 15:54:08 +0530

    Adding addiotional Test cases for test harness

--- src/ApplicationTest.java
+++ src/ApplicationTest.java
@@ -1,8 +1,15 @@
+import java.beans.Transient;
+
 import org.junit.jupiter.api.Test;
 
 public class ApplicationTest {
     @Test
     public void FirstTestCase() {
         // First Test case added after the project Initialize
+    }
+
+    @Test
+    public void SecondTestCase(){
+        // Second Test case by @TesterOne for the test harness
     }
 }
