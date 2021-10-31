458ae9bab79ae3f520b03f28f16966c71aaf74e7
Team Leader <teamleader@automation.com>
2021-10-31 15:25:25 +0530

    Adding first automation test cases for Main Application

--- src/Application.java
+++ src/Application.java
@@ -1,1 +1,6 @@
-public class Application{}
+public class Application{
+
+    public static void main(String[] args) {
+        System.out.println("Application Launched OK");
+    }
+}
--- src/ApplicationTest.java
+++ src/ApplicationTest.java
@@ -1,3 +1,8 @@
+import org.junit.jupiter.api.Test;
 
 public class ApplicationTest {
+    @Test
+    public void FirstTestCase() {
+        // First Test case added after the project Initialize
+    }
 }
