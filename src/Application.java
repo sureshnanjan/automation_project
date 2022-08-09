f020c631fcc0044b6f1b168d8419cd8ba27f5062
Suresh Nanjan <85288946+sureshnanjan@users.noreply.github.com>
2022-08-09 15:49:17 +0530

    Changes by Tester 2
    This is to add new feature files to test the BinarySearch

--- src/Application.java
+++ src/Application.java
@@ -3,4 +3,8 @@ public class Application{
     public static void main(String[] args) {
         System.out.println("Application Launched OK");
     }
+    
+    private static void Tester2Method(){
+        System.out.println("Application Launched OK");
+    }
 }
